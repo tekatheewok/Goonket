c5b02e32bde39b60d5a878a79708581b9ef19aaf
dxpressedskittle <hinuto026@gmail.com>
2025-03-08 14:54:21 -0500

    s

--- js/register.js
+++ js/register.js
@@ -44,6 +44,8 @@ form.addEventListener('submit', async (e) => {
     messageBox.style.opacity = 1;
 }, 50);
 
+
+
 document.getElementById("submit").onclick = function() {
     var newDiv = document.createElement("div");
     newDiv.className = "message-box"
