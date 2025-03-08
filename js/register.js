25519fb2f9f328023c3b5ad173ada9bb7c8ec6b5
tekatheewok <happygamer4700@gmail.com>
2025-03-08 03:05:49 +0000

    s

--- js/register.js
+++ js/register.js
@@ -5,18 +5,20 @@ form.addEventListener('submit', async (e) => {
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
 
     try {
         const response = await axios.post('https://api.goonket.com/register', {
             username,
-            password
+            password,
+            email
         });
 
         alert('User registered successfully!');
         console.log(response.data); 
        
     } catch (error) {
-        console.error('Error registering:', error.response.data);
-        alert('Username or email taken, Please try again!');
+        const errorMessage = error.response && error.response.data ? error.response.data.message : 'An error occurred. Please try again!';
+        alert(errorMessage)
     }
 });
