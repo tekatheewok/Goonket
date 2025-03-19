9fd3941473a0c1da7374a861017169f6e50429dc
alex <129445432+nzvsky@users.noreply.github.com>
2025-03-19 23:41:44 +0000

    asdasd

--- _REACT-DEV/goonket/src/jsx/global.jsx
+++ _REACT-DEV/goonket/src/jsx/global.jsx
@@ -1,14 +1,5 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-
 export function PageButton({text, destination}) {
     return (
         <button onClick={() => {window.location.href = destination}}>{text}</button>
     );
 };
-
-export function Gooner() {
-    return (
-        <h1>DXPRESSEDSKITTLE LOVES JASON MURRRRRRR</h1>
-    );
-}
--- _REACT-DEV/goonket/src/jsx/home.jsx
+++ _REACT-DEV/goonket/src/jsx/home.jsx
@@ -1,14 +1,30 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { PageButton, Gooner } from "./global.jsx"
+import { PageButton } from "./global.jsx"
 import "../style.css"
+
+function Header() {
+  return (
+    <div id="header" className="content-section center-content vertical-stack">
+      <h1 id="header-title" className="header-item">Goonket</h1>
+      <p id="header-desc" className="header-item">Goonket is a site that combines gooning with learning.</p>
+    </div>
+  )
+}
+
+function MainContent() {
+  return (
+    <div className="content-section center-content vertical-stack">
+      <h1>Ready to play?</h1>
+      <PageButton text="Log in" destination="/login"/>
+      <PageButton text="Sign up" destination="/register"/>
+    </div>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div>
-      <h1>Goonket</h1>
-      <PageButton text="test" destination="https://example.com"/>
-      <Gooner/>
-    </div>
+    <Header/>
+    <MainContent/>
   </StrictMode>
 )
