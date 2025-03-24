21633ddca82feb79e5520055071ae31d9c350a9a
alex <129445432+nzvsky@users.noreply.github.com>
2025-03-24 14:41:59 +0000

    fsfdasd

--- _REACT-DEV/goonket/jsx/home.jsx
+++ _REACT-DEV/goonket/jsx/home.jsx
@@ -4,9 +4,15 @@ import { PageButton } from "./global.jsx"
 
 function Header() {
   return (
-    <div id="header" className="content-section center-content vertical-stack">
-      <h1 id="header-title" className="header-item">Goonket</h1>
-      <p id="header-desc" className="header-item">Goonket is a site that combines gooning with learning.</p>
+    <div id="header" className="content-section center-content">
+      <div class="content-section center-content vertical-stack header-content-section">
+        <h1 id="header-title" className="header-item">Goonket</h1>
+        <p id="header-desc" className="header-item">Goonket is a site that combines learning with play.</p>
+      </div>
+      <div class="content-section center-content vertical-stack header-content-section">
+        <button>Log in</button>
+        <button>Sign up</button>
+      </div>
     </div>
   )
 }
@@ -14,9 +20,7 @@ function Header() {
 function MainContent() {
   return (
     <div className="content-section center-content vertical-stack">
-      <h1>Ready to play?</h1>
-      <PageButton text="Log in" destination="/login"/>
-      <PageButton text="Sign up" destination="/register"/>
+      <h1>goonket is awesome</h1>
     </div>
   )
 }
